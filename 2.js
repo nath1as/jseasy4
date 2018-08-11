a1d2328b770c98f8fd366bcd54517d0958c7f6cc
nathias <n@th1.as>
2018-08-11 19:01:08 +0200

    linter with 2spaces

--- 2.js
+++ 2.js
@@ -1,17 +1,17 @@
 function union(array1, array2) {
-    var newArray = [];
-    var i;
-    for (i = 0; i < array1.length; i++) {
-        if (newArray.indexOf(array1[i]) == -1) {
-            newArray.push(array1[i]);
-        }
+  var newArray = [];
+  var i;
+  for (i = 0; i < array1.length; i++) {
+    if (newArray.indexOf(array1[i]) == -1) {
+      newArray.push(array1[i]);
     }
-    for (i = 0; i < array2.length; i++) {
-        if (newArray.indexOf(array2[i]) == -1) {
-            newArray.push(array2[i]);
-        }
+  }
+  for (i = 0; i < array2.length; i++) {
+    if (newArray.indexOf(array2[i]) == -1) {
+      newArray.push(array2[i]);
     }
-    console.log(newArray);
+  }
+  console.log(newArray);
 }
 
 union([1, 3, 5], [3, 6, 9]);    // [1, 3, 5, 6, 9]
