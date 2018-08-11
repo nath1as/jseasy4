d7205ec9623311ab886d1819f8f635cd6ffea32d
nathias <n@th1.as>
2018-08-11 18:57:27 +0200

    linter

--- 1.js
+++ 1.js
@@ -3,19 +3,19 @@ var MINUTES_PER_DEGREE = 60;
 var SECONDS_PER_DEGREE = 3600;
 
 function dms(degreesFloat) {
-  var degreesInt = Math.floor(degreesFloat);
-  var minutes = Math.floor((degreesFloat - degreesInt) * MINUTES_PER_DEGREE);
-  var seconds = Math.floor(
-    (degreesFloat - degreesInt - (minutes / MINUTES_PER_DEGREE)) *
-    SECONDS_PER_DEGREE
-  );
-  return String(degreesInt) + DEGREE + padZeroes(minutes) + "'" +
-                                       padZeroes(seconds) + '"';
+    var degreesInt = Math.floor(degreesFloat);
+    var minutes = Math.floor((degreesFloat - degreesInt) * MINUTES_PER_DEGREE);
+    var seconds = Math.floor(
+        (degreesFloat - degreesInt - (minutes / MINUTES_PER_DEGREE)) *
+        SECONDS_PER_DEGREE
+    );
+    return String(degreesInt) + DEGREE + padZeroes(minutes) + "'" +
+                                         padZeroes(seconds) + '"';
 }
 
 function padZeroes(number) {
-  var numString = String(number);
-  return numString.length < 2 ? ('0' + numString) : numString;
+    var numString = String(number);
+    return numString.length < 2 ? ('0' + numString) : numString;
 }
 
 console.log(dms(30));           // 30°00'00"
